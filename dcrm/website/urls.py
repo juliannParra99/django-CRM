e702ed9aee34d45574fd304190c484844070e5ca
julianParrax <julianfranco.parra@gmail.com>
2023-08-29 20:09:01 -0300

    Added:4. Logout users with django method + navbar with login and logout

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -5,6 +5,6 @@ from . import views
 # //estas son mis rutas que se van a mapear en una vista especifica.
 urlpatterns = [
     path('', views.home, name="home"),
-    # path('login/', views.home, name="login"),
-    # path('logout/', views.home, name="logout"),
+    # path('login/', views.login_user, name="login"), esta comentada, pero es en caso de querer crear un nuevo metodo que renderize otra pagina y no se realize todo el la pagina de home. En caso de querer hacerlo solo tengo que hacer lo mismo que esta en el metodo de home
+    path('logout/', views.logout_user, name="logout"),
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -8,26 +8,32 @@ from django.contrib import messages
 def home(request):
     # Verificar si la solicitud es un método POST (es decir, se está enviando un formulario)
     if request.method == 'POST':
-        # Obtener el nombre de usuario y la contraseña desde los datos enviados en el formulario POST; username y pasword son los nombres que le asignamos los elementos en el formulario
         username = request.POST['username']
         password = request.POST['password']
 
         # Autenticar al usuario utilizando el nombre de usuario y contraseña proporcionados
         user = authenticate(request, username=username, password=password)
 
-        # Verificar si la autenticación fue exitosa]: verifica si los datos ingresados corresponden con un usuario en la base de datos.Devuelve un objeto si coincide, entonces si la variable user no esta vacia hay un objeto y por lo tanto la autenticacion fue exitosa
+        
         if user is not None:
             # Iniciar sesión al usuario autenticado
             login(request, user)
-            # Agregar un mensaje de éxito a la cola de mensajes para mostrar al usuario
+            
             messages.success(request, "You Have Been Logged In!")
-            # Redirigir al usuario a la página de inicio (para mostrar la lista de registros)
+            
             return redirect('home')
         else:
-            # Si la autenticación falla, agregar un mensaje de error a la cola de mensajes
+            
             messages.success(request, "There Was An Error Logging In, Please Try Again...")
             # Redirigir de nuevo a la página de inicio
             return redirect('home')
     else:
-        # Si la solicitud no es un método POST, renderizar la plantilla 'home.html' y pasar los registros a la plantilla
+        
         return render(request, 'home.html', {})
+    
+# la funcion logout de django se encarga de toda la logica.
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect('home')
+      
